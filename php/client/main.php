65948c02b3677d0d31de1c4e3839b76d85026266
ArcPro <72943767+ArcPro@users.noreply.github.com>
2023-06-02 22:25:09 +0200

    update main

--- php/client/main.php
+++ php/client/main.php
@@ -151,34 +151,56 @@ body {
           </div>
         </div>
       </div>
-      <div class="carousel-item active">
+      <?php
+      if (!isset($_SESSION["uuid"])) {
+        echo '
+        <div class="carousel-item active">
         <img src="image/slide1.jpg" style="height:500px;">
         <div class="container">
           <div class="carousel-caption">
             <h1>Bienvenue.</h1>
-            <p>Rejoignez votre site d'echecs en ligne avec plus de <?php echo TotalMembers()?> membres actifs !</p>
+            <p>Rejoignez votre site d\'echecs en ligne avec plus de ' . TotalMembers() . 'membres actifs !</p>
             <p><a class="btn btn-lg btn-primary" href="#" role="button" style="margin-bottom:100px;">Rejoignez nous</a></p>
           </div>
         </div>
       </div>
+        ';
+      }
+      else {
+        echo '
+        <div class="carousel-item active">
+        <img src="image/slide1.jpg" style="height:500px;">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Faites une partie.</h1>
+            <p>Jouez à votre site d\'echecs en ligne avec plus de ' . TotalMembers() . ' membres actifs !</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button" style="margin-bottom:100px;">Jouer</a></p>
+          </div>
+        </div>
+      </div>
+        ';
+      }
+
+      ?>
+      
       <div class="carousel-item">
         <img src="image/slide2.jpg" style="height:500px;">
         <div class="container">
           <div class="carousel-caption text-right">
-            <h1>One more for good measure.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button" style="margin-bottom:100px;">Browse gallery</a></p>
+            <h1>Classement.</h1>
+            <p>Un classement est désormais disponible, il prend en compte votre ratio de victoires pour déterminer votre place.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button" style="margin-bottom:100px;">Parcourir le classement</a></p>
           </div>
         </div>
       </div>
     </div>
     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
+      <span class="sr-only">Suivant</span>
     </a>
     <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
+      <span class="sr-only">Précédent</span>
     </a>
   </div>
   </main>
